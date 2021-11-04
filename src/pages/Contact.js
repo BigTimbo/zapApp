3ce52cc59705a0f85185b2bc958f23f2dbf3ac0b
Timbo <timothy_amis@msn.com>
2021-11-04 15:50:45 +0000

    tweaking min-heights

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,9 @@ function Contact() {
     return (
         <div className="contact">
             <div className="contactContent">
+                <div className="contactForm">
 
+                </div>
             </div>
         </div>
     )
