5f90e1b1f067cbbf4528971bb66dc7a99926e898
Timbo <timothy_amis@msn.com>
2021-11-23 10:15:29 +0000

    map generating pins for sightings

--- src/pages/Sightings.js
+++ src/pages/Sightings.js
@@ -11,7 +11,8 @@ class Sightings extends React.Component {
         super(props);
         this.state = {
             content : null,
-            loading : true
+            loading : true,
+            sightingsMap : null,
         }
     }
     async componentDidMount(){
@@ -44,7 +45,12 @@ class Sightings extends React.Component {
     }
     buildTable(json){
         const content = [];
+        const mapContent = [];
         for (let i = 0; i < json.sightings.length; i++) {
+            let location = json.sightings[i].location;
+            location = JSON.parse(location);
+            const pin = `pin-s-${json.sightings[i].ID}+555555(${location.lng},${location.lat})`;
+            mapContent.push(pin);
             content.push(
                 <tr key={json.sightings[i].ID}>
                     <td>{json.sightings[i].ID}</td>
@@ -54,6 +60,8 @@ class Sightings extends React.Component {
                 </tr>
             );
         }
+        console.log(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${mapContent.toString()}/23.5713,-28.6876,5.1,0/1280x720?access_token=pk.eyJ1IjoiYmlndGltYm8iLCJhIjoiY2t3YWcxeTMwMXM0NzJ2cmg4b2o0YnhsaiJ9.JZWpZWaRPgctxFfIP0Vsqw`);
+        this.setState({sightingsMap : `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${mapContent.toString()}/23.5713,-28.6876,5.1,0/1280x720?access_token=pk.eyJ1IjoiYmlndGltYm8iLCJhIjoiY2t3YWcxeTMwMXM0NzJ2cmg4b2o0YnhsaiJ9.JZWpZWaRPgctxFfIP0Vsqw`})
         this.setState({content : content});
     }
     render(){
@@ -62,26 +70,26 @@ class Sightings extends React.Component {
                 <img id="loading" src={Loading} width="50px" height="50px" alt="Loading Animation"/>
             </div>
             :
-            ""
+            <div className="sightingsContent">
+                <img src={this.state.sightingsMap} width="1280px" height="720px" className="map"  alt="Sightings map" />
+                <table id="sightingsTable">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Alive</th>
+                        <th>Cause Of Death</th>
+                        <th>Notes</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.content}
+                    </tbody>
+                </table>
+            </div>
         ;
         return (
             <div className="sightings">
-                <div className="sightingsContent">
-                    <table id="sightingsTable">
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Alive</th>
-                            <th>Cause Of Death</th>
-                            <th>Notes</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.content}
-                        </tbody>
-                    </table>
-                    {loading}
-                </div>
+                {loading}
             </div>
         )
     }
